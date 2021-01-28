d2e56c9202423fb981fef6db697d9399e6ff80f5
callkend <77285430+callkend@users.noreply.github.com>
2021-01-28 09:01:14 -0700

    Add project files.

---
+++ AccumulateMessagesFunction.vb
@@ -0,0 +1,39 @@
+﻿
+
+
+Option Explicit On
+Option Strict On
+
+Module AccumulateMessagesFunction
+    Sub Main()
+        Dim userInput As String
+        Dim storedMessages As String
+
+        Console.WriteLine("Write a Message to store or write clear to clear stored messages")
+        For i = 0 To 5
+            userInput = Console.ReadLine()
+            If userInput = "clear" Then
+                storedMessages = UserMessages(userInput, True)
+            Else
+                storedMessages = UserMessages(userInput + vbNewLine, False)
+            End If
+
+        Next
+
+        Console.WriteLine(storedMessages)
+        Console.ReadLine()
+    End Sub
+
+    Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
+        Static storedMessages As String
+
+        If clear = False Then
+            storedMessages &= newMessage
+        Else
+            storedMessages = String.Empty
+        End If
+
+        Return storedMessages
+    End Function
+
+End Module
